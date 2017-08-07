d27bf6c04f1f79544f0df9f985e1d4f5a2fa8b2f
rory payne <rorypayne@rorys-MacBook.local>
2017-08-07 20:40:11 +0100

    first commit

--- src/app/main/ts/service/change-rest.service.ts
+++ src/app/main/ts/service/change-rest.service.ts
@@ -1,7 +1,6 @@
 import {Http, Headers} from "@angular/http";
 import {environment} from "../../../../environments/environment";
 import {AuthService} from "./auth.service";
-import {TokenService} from "./token.service";
 import {Injectable} from "@angular/core";
 
 @Injectable()
@@ -11,10 +10,7 @@ export class ChangeRestService {
 
   sendGet(url: string, config: any): Promise<any> {
     return this.authService.getAccessToken().then(token => {
-      if (!config.headers) {
-        config.headers = new Headers();
-      }
-      config.headers.append('Authorization', `bearer ${token}`);
+      this.setAuthHeader(config, token);
       return this.http.get(environment.serviceBaseUrl + url, config)
         .toPromise().then(null, err => {
           if (err.status === 401) {
@@ -26,22 +22,53 @@ export class ChangeRestService {
     });
   }
 
+  private setAuthHeader(config: any, token) {
+    if (!config.headers) {
+      config.headers = new Headers();
+    }
+    config.headers.append('Authorization', `bearer ${token}`);
+  }
+
   sendPost(url: string, data: any, config: any): Promise<any> {
-    return this.http.post(environment.serviceBaseUrl + url, JSON.stringify(data), config)
-      .toPromise()
-      .catch(this.handleError);
+    return this.authService.getAccessToken().then(token => {
+      this.setAuthHeader(config, token);
+      return this.http.post(environment.serviceBaseUrl + url, JSON.stringify(data), config)
+        .toPromise().then(null, err => {
+          if (err.status === 401) {
+            this.authService.authenticate()
+              .then(() => this.sendGet(url, config));
+          }
+        })
+        .catch(this.handleError);
+    });
   }
 
   sendPut(url: string, data: any, config: any): Promise<any> {
-    return this.http.put(environment.serviceBaseUrl + url, JSON.stringify(data), config)
-      .toPromise()
-      .catch(this.handleError);
+    return this.authService.getAccessToken().then(token => {
+      this.setAuthHeader(config, token);
+      return this.http.put(environment.serviceBaseUrl + url, JSON.stringify(data), config)
+        .toPromise().then(null, err => {
+          if (err.status === 401) {
+            this.authService.authenticate()
+              .then(() => this.sendGet(url, config));
+          }
+        })
+        .catch(this.handleError);
+    });
   }
 
   sendDelete(url: string, config: any): Promise<any> {
-    return this.http.get(environment.serviceBaseUrl + url, config)
-      .toPromise()
-      .catch(this.handleError);
+    return this.authService.getAccessToken().then(token => {
+      this.setAuthHeader(config, token);
+      return this.http.get(environment.serviceBaseUrl + url, config)
+        .toPromise().then(null, err => {
+          if (err.status === 401) {
+            this.authService.authenticate()
+              .then(() => this.sendGet(url, config));
+          }
+        })
+        .catch(this.handleError);
+    });
   }
 
   private handleError(error: any): Promise<any> {
